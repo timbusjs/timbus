e82259e8324c9e448d6605b2072fd18827b4bd43
Braden Hitchcock <developer.hitchcock@gmail.com>
2020-03-09 09:27:03 -0700

    refactor: DRY uid

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,50 +3,56 @@ const express = require('express');
 function buildHomeRoute(config) {
   const router = express.Router();
 
-  router.get('/', async function(req, res) {
-    // Get all of the assigments
-    const assignments = await req.store.assignments();
+  router.get('/', async function(req, res, next) {
+    try {
+      const { uid } = req.session;
 
-    // Get all of the file submission for the user
-    const uploads = await req.store.get({ uid: req.session.uid });
+      // Get all of the assigments
+      const assignments = await req.store.assignments();
 
-    // Create our submissions to render
-    const submissions = assignments.value.map(assignment => {
-      let uploadedAssignment = uploads.value.filter(upload => upload.aid === assignment.aid);
-      let isSubmitted = uploadedAssignment.length !== 0;
-      if (isSubmitted) uploadedAssignment = uploadedAssignment[0];
-      return {
-        aid: assignment.aid,
-        name: assignment.name,
-        submissionDate: isSubmitted ? uploadedAssignment.submissionDate : '',
-        isVerified: isSubmitted ? (uploadedAssignment.isVerified ? 'Yes' : 'No') : '',
-        isSubmitted,
-        downloadLink: isSubmitted ? '/api/uploads/' + req.session.uid + '/' + assignment.aid : ''
+      // Get all of the file submission for the user
+      const uploads = await req.store.get({ uid });
+
+      // Create our submissions to render
+      const submissions = assignments.map(assignment => {
+        let uploadedAssignment = uploads.filter(upload => upload.aid === assignment.aid);
+        let isSubmitted = uploadedAssignment.length !== 0;
+        if (isSubmitted) uploadedAssignment = uploadedAssignment[0];
+        return {
+          aid: assignment.aid,
+          name: assignment.name,
+          submissionDate: isSubmitted ? uploadedAssignment.submissionDate : '',
+          isVerified: isSubmitted ? (uploadedAssignment.isVerified ? 'Yes' : 'No') : '',
+          isSubmitted,
+          downloadLink: isSubmitted ? '/api/uploads/' + uid + '/' + assignment.aid : ''
+        };
+      });
+
+      let isAdmin = req.session.role === 'admin';
+
+      const context = {
+        user: {
+          uid,
+          name: req.session.name,
+          isAdmin
+        },
+        admin: {
+          navText: isAdmin ? 'View All Submissions' : '',
+          navLink: isAdmin ? '/submissions' : ''
+        },
+        header: {
+          subtext: config.view.subtext
+        },
+        body: {
+          assignments,
+          submissions
+        }
       };
-    });
 
-    let isAdmin = req.session.role === 'admin';
-
-    const context = {
-      user: {
-        uid,
-        name: req.session.name,
-        isAdmin
-      },
-      admin: {
-        navText: isAdmin ? 'View All Submissions' : '',
-        navLink: isAdmin ? '/submissions' : ''
-      },
-      header: {
-        subtext: config.view.subtext
-      },
-      body: {
-        assignments,
-        submissions
-      }
-    };
-
-    res.render('home', context);
+      res.render('home', context);
+    } catch (err) {
+      next(err);
+    }
   });
 
   return router;
