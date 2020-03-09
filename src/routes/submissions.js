4d18ecf4507a70a28383506b291fdfca3c085f81
Braden Hitchcock <developer.hitchcock@gmail.com>
2020-03-09 10:13:29 -0700

    fix: broken submission view handling from refactor

--- src/routes/submissions.js
+++ src/routes/submissions.js
@@ -11,7 +11,21 @@ function buildSubmissionRoutes(config) {
       }
 
       // Get all of the submissions
-      let assignments = await req.store.get();
+      const submissions = await req.store.get();
+
+      // Organize them by assignment
+      let assignments = await req.store.assignments();
+      assignments = assignments.map(a => {
+        a.submissions = [];
+        for (const s of submissions) {
+          if (s.aid === a.aid) {
+            s.downloadLink = '/api/uploads/' + s.uid + '/' + s.aid;
+            a.submissions.push(s);
+          }
+        }
+        return a;
+      });
+
       res.render('submissions', {
         user: {
           uid: req.session.uid,
