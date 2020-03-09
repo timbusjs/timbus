c6353ebd2a43e70a5491de420f4f9ff04eeb8c8d
Braden Hitchcock <developer.hitchcock@gmail.com>
2020-03-09 09:27:21 -0700

    fix: match new context structure

--- src/routes/submissions.js
+++ src/routes/submissions.js
@@ -3,21 +3,32 @@ const express = require('express');
 function buildSubmissionRoutes(config) {
   const router = express.Router();
 
-  router.get('/submissions', async function(req, res) {
-    const isAdmin = req.session.role === 'admin';
-    if (!isAdmin) {
-      return res.render('401', { unauthorized: true });
-    }
+  router.get('/submissions', async function(req, res, next) {
+    try {
+      const isAdmin = req.session.role === 'admin';
+      if (!isAdmin) {
+        return res.render('401', { unauthorized: true });
+      }
 
-    // Get all of the submissions
-    let assignments = await req.store.get();
-    res.render('submissions', {
-      uid: req.session.uid,
-      isAdmin,
-      assignments,
-      adminNavText: 'View My Submissions',
-      adminNavLink: '/'
-    });
+      // Get all of the submissions
+      let assignments = await req.store.get();
+      res.render('submissions', {
+        user: {
+          uid: req.session.uid,
+          name: req.session.name,
+          isAdmin: req.session.role === 'admin'
+        },
+        admin: {
+          navText: 'View My Submissions',
+          navLink: '/'
+        },
+        body: {
+          assignments
+        }
+      });
+    } catch (err) {
+      next(err);
+    }
   });
 
   return router;
